cd79569b1091c7a5207d23521df401e45368edf8
Berenice <berenicelopesmaria@gmal.com>
2021-11-30 12:03:55 -0300

    listando todas as vacinas

--- src/routes/vaccines.js
+++ src/routes/vaccines.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const controller = require("../controllers/vaccineController")
 
 router.post("/", controller.createVaccine)
+router.get("/", controller.getAllVaccines)
 
 module.exports = router
