754a5752e29af5f317c34fc039d5e09b6f9866b4
Berenice <berenicelopesmaria@gmal.com>
2021-12-01 11:11:55 -0300

    implementação final

--- src/app.js
+++ src/app.js
@@ -10,9 +10,19 @@ app.use(function(req, res, next)  {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
-         "Origin, XRequested-With, Content-Type, Accept"
-    );
+         "Origin, X-Requested-With, Content-Type, Accept"
+    )
     next();
+});
+
+app.options("/*", (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers")
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT,POST,GET,DELETE,OPTIONS,PATCH"
+    )
+    res.send("send some thing whatever")
 });
 
 app.use("/", index);
--- src/routes/vaccines.js
+++ src/routes/vaccines.js
@@ -1,11 +1,11 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/vaccineController")
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/vaccineController");
 
-router.post("/", controller.createVaccine)
-router.get("/", controller.getAllVaccines)
-router.get("/:id", controller.getVaccine)
-router.put("/:id", controller.updateVaccine)
-router.patch("/:id/vaccinated", controller.updateVaccinated)
+router.post("/", controller.createVaccine);
+router.get("/", controller.getAllVaccines);
+router.get("/:id", controller.getVaccine);
+router.put("/:id", controller.updateVaccine);
+router.patch("/:id/vaccinated", controller.updateVaccinated);
 
 module.exports = router
